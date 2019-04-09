45b2650bdc9b5bab85cd6558a290f1fda0c3dbcb
Sergey Privalov <kalabanga12@yandex.ru>
2019-04-09 23:02:13 +0500

    Второй урок

--- js/script.js
+++ js/script.js
@@ -95,6 +95,81 @@ more.addEventListener('click', () => {
     videosWrapper.appendChild(card);  //Добавляем в конец каждой карточки новую
     setTimeout(() => {
       card.classList.remove('videos__item-active');   //устанавливаем время появления карточек
-    }, 10);    
+    }, 10);
+    bindNewModal(card);    
+  }
+
+  sliceTitle('.videos__item-descr', 80);
+});
+
+function sliceTitle(selector, count) {   //Подрезаем заголовок
+  document.querySelectorAll(selector).forEach(item => {
+    item.textContent.trim();  //Удаляет лишние пробелы
+
+    if (item.textContent.length < count) {  //Кол-во символов < 100
+      return;
+    } else {
+      const str = item.textContent.slice(0, count + 1) + "..."; // Обрезаем строку
+      item.textContent = str;
+    }
+  });
+}
+sliceTitle('.videos__item-descr', 80);
+
+function openModal(){
+  modal.style.display = 'block';
+}
+
+function closeModal(){
+  modal.style.display = 'none';
+  player.stopVideo();
+}
+
+function bindModal(cards) {
+  cards.forEach(item => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = item.getAttribute('data-url');
+      loadVideo(id);
+      openModal();
+    });
+  });
+}
+bindModal(videos);
+
+function bindNewModal(cards) {
+  cards.addEventListener('click', (e) => {  //Откр-ет модальное окно одной карточки
+    e.preventDefault();
+    const id = cards.getAttribute('data-url');
+      loadVideo(id);
+    openModal();
+  });
+}
+
+modal.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('modal__body')) {  //Если кликаем не по модальному окну
+    closeModal();
   }
 });
+
+function createVideo(){
+  var tag = document.createElement('script');   //https://developers.google.com/youtube/iframe_api_reference?hl=ru
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  setTimeout(() => {                      //Делаем задержку
+    player = new YT.Player('frame', {    //Создаём новый экземпляр плеера
+      height: '100%',                      //Настройки
+      width: '100%',
+      videoId: 'M7lc1UVf-VE'
+    });
+  }, 300);
+}
+
+createVideo();
+
+function loadVideo(id) {    //Загружаем новое видео по id
+  player.loadVideoById({'videoId': `${id}`});
+}
